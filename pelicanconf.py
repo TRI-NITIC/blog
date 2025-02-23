dafc33f6bb8cd196a16880b2f8ab31ae378fbd8d
tatsuki-s <zekoushn@gmail.com>
2025-02-23 18:01:51 +0900

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = 'TRI'
+SITENAME = 'TRI-Blog'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+THEME = 'themes/aboutwilson'
+
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("X(Twitter)", "https://x.com/NITIC_Giken"),
+    ("Instagram", "https://www.python.org/"),
+    ("YouTube", "https://palletsprojects.com/p/jinja/")
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
